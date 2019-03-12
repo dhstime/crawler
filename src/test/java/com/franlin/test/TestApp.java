e7ccdb629fa360a4db791cc26b8a9b5be595c915
franlin.wu <it8086@vedeng.com>
2019-03-12 10:24:09 +0800

    测试代码

---
+++ src/test/java/com/franlin/test/TestApp.java
@@ -0,0 +1,14 @@
+
+package com.franlin.test;
+/**
+ * <b>Description:</b><br> 
+ * <b>Author: Franlin.wu</b> 
+ * @fileName TestApp.java
+ * <br><b>Date: 2019年3月12日 上午10:23:33 </b> 
+ *
+ */
+public class TestApp
+{
+
+}
+
